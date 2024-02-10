3558e5bdf728f3f3b2fa55d5558a240f9f792e53
Vladimir Babin <vovababin@gmail.com>
2024-02-10 15:35:14 +0300

    Add PYTHONUNBUFFERED to env

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ fi
 
 sudo docker build -t wiki . \
 && (sudo docker stop wiki || true && sudo docker rm wiki || true) \
-&& sudo docker run -d -v $(pwd)/posted.txt:/app/posted.txt \
+&& sudo docker run -d \
+   -v $(pwd)/posted.txt:/app/posted.txt \
+   -e PYTHONUNBUFFERED=1 \
    --name wiki --restart unless-stopped wiki \
 && sudo docker logs -f wiki
